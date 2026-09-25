4fd4698aae86109ce3dd88770536f06ef47a01d4
Кирилл Соловьев <k.solovev994@yandex.ru>
2026-09-25 16:35:53 +0300

    fix main

--- src/main/java/ru/mirea/railway/Main.java
+++ src/main/java/ru/mirea/railway/Main.java
@@ -1,4 +1,18 @@
-package main.java.ru.mirea.project;
+package ru.mirea.railway;
 
+import ru.mirea.railway.ui.ConsoleMenu;
+
+/**
+ * Точка входа в приложение.
+ */
 public class Main {
+
+    public static void main(String[] args) {
+        try {
+            new ConsoleMenu().run();
+        } catch (Exception e) {
+            System.err.println("Критическая ошибка: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
